056cc175edbea8350184dd3d222be6b1f043c27e
Saranyu <saranyu.sst@gmail.com>
2019-03-28 17:47:47 +0700

    update

--- angularApp/src/app/app-routing.module.ts
+++ angularApp/src/app/app-routing.module.ts
@@ -1,24 +1,7 @@
 
-// import { Routes, RouterModule } from '@angular/router';
-
-// import { AppComponent } from "./app.component";
-
-// const routes: Routes = [
-//     { path: '', redirectTo: 'home', pathMatch: 'full' },
-//     { path: "home", loadChildren: './home/home.module#HomeModule' },
-//     { path: "profile", loadChildren: './profile/profile.module#ProfileModule' }
-// ];
-
-
-// export const AppRoutes = RouterModule.forRoot(routes, { enableTracing: true });
-
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from "./app.component";
-import {HomeComponent} from "./home/home.component";
-import {MemoSearchComponent} from "./memo-search/memo-search.component";
-import {MemoMasterComponent} from "./memo-master/memo-master.component";
-import {MemoDetailComponent} from "./memo-detail/memo-detail.component";
 const routes: Routes = [
    
 ];
